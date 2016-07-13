97e354c5ac1864a54315838dc20162884b02972f
Jasper De Rocker <jasper.derocker@gmail.com>
2016-07-13 15:41:07 +0200

    updated databaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        $this->call(GeolocationSeeder::class);
+        //$this->call(GeolocationSeeder::class);
+        //$this->call(CategorySeeder::class);
         $this->call(PlaceSeeder::class);
     }
 }
@@ -55,6 +56,22 @@ class GeolocationSeeder extends Seeder
     }
 }
 
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {            
+            DB::table('categories')->insert([['name' => 'park', 'foursquareId' => '4bf58dd8d48988d163941735', 'icon' => 'null'],
+                                             ['name' => 'coffee shop', 'foursquareId' => '4bf58dd8d48988d1e0931735', 'icon' => 'null'],
+                                             ['name' => 'college library', 'foursquareId' => '4bf58dd8d48988d1a7941735', 'icon' => 'null'],
+                                             ['name' => 'library', 'foursquareId' => '4bf58dd8d48988d12f941735', 'icon' => 'null']]);
+    }
+}
+
 class PlaceSeeder extends Seeder
 {
 
@@ -82,12 +99,40 @@ class PlaceSeeder extends Seeder
         $venues = $response->venues;
 
         foreach($venues as $venue){
-            
-            $geo = DB::table('geolocations')->where('longitude',$venue->location->lng)
-                ->where('latitude',$venue->location->lat)->first();
-            DB::table('places')->insert([['name' => $venue->name,
+
+            $fullvenue = json_decode(file_get_contents(
+            'https://api.foursquare.com/v2/venues/'.$venue->id
+            .'?client_id='.env('FOURSQUARE_CLIENT_ID','')
+            .'&client_secret='.env('FOURSQUARE_CLIENT_KEY','')
+            .'&ll=50.8798,4.7005'
+            .'&v=20150806&m=foursquare'
+            .'&radius=5000'
+            .'&intent=checkin'
+            .'&categoryId='.$this::CATEGORY_IDS
+            .'&near='.$this::LOCATION))->response->venue;
+
+                       
+            $geo = DB::table('geolocations')->where('longitude',$fullvenue->location->lng)
+                ->where('latitude',$fullvenue->location->lat)->first();
+
+            //     printf($fullvenue->location->formattedAddress[0]);
+
+            $catfoursquareid = $fullvenue->categories[0]->id;
+            $description = property_exists($fullvenue,'description') ? $fullvenue->description : ''; 
+            $category = DB::table('categories')->where('foursquareId',$catfoursquareid)->first();
+            $categoryid = is_object($category) ? $category->id : null;
+            $url = property_exists($fullvenue, 'url') ? $fullvenue->url : '';
+
+
+
+            DB::table('places')->insert([['name' => $fullvenue->name,
                                           'geoId' => $geo->id,
-                                          'foursquareId' => $venue->id]]);
+                                          'foursquareId' => $fullvenue->id,
+                                          'description' => '',
+                                          'email' => '',
+                                          'site' => $url,
+                                          'categoryId' => $categoryid,
+                                          'address' => $fullvenue->location->formattedAddress[0]]]);
         }
     }
 }
