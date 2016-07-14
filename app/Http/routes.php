100a7dfb3a6ed8818d77f7cb729ce49cab8f0023
thibautvincent <thibautvincent@icloud.com>
2016-07-14 16:00:17 +0200

    Deleting old routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,12 +17,10 @@ Route::get('/', function () {
 });
 
 // Authentication routes...
-Route::get('auth/login', 'Auth\AuthController@getLogin');
 Route::post('auth/login', 'Auth\AuthController@postLogin');
 Route::get('auth/logout', 'Auth\AuthController@logout');
 
 // Registration routes...
-Route::get('auth/register', 'Auth\AuthController@getRegister');
 Route::post('auth/register', 'Auth\AuthController@postRegister');
 
 // oAuth routes
