a7db2aaa3c71e95716799b6240bc692cf5dead8e
AngryMoustache <mrclotion@hotmail.com>
2016-07-18 16:15:15 +0200

    Fix in CheckinController

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,9 @@
     <body>
     	<div id="content">
 	    	<h1>LeuvenLeest - API</h1>
+	    	<h2>Login and registration</h2>
+	    	<hr>
+
 	    	<h2>Basic usage - GET</h2>
 	    	<p class="quote">
 				var settings = {<br>
@@ -31,24 +34,59 @@
 	    	<hr>
 
 	    	<h2>#Users</h2>
-	    	<p><strong>getUserById</strong>: Get user by ID, you only need to pass the ID<br></p>
+
+	    	<p>
+	    		<strong>getUserById</strong><br>
+	    		Get user by ID, you only need to pass the ID<br>
+	    		Returns users:<br>
+	    		<i>id</i>, <i>name</i>, <i>email</i>, <i>password</i>, <i>remember_token</i>, <i>created_at</i> and <i>updated_at</i>.
+	    	</p>
 	    	<p class="quote">"url": "http://95.85.15.210/user/get/<strong>USER_ID</strong>",</p>
+	    	
 	    	<hr>
 
 	    	<h2>#Places</h2>
-	    	<p><strong>getPlaces</strong>: Get the places ordered by distance (in km) by passing latitude and longitude<br></p>
+
+	    	<p>
+	    		<strong>getPlaces</strong><br>
+	    		Get the places ordered by distance (in km) by passing latitude and longitude<br>
+	    		Returns all places:<br>
+	    		<i>id</i>, <i>foursquareId</i>, <i>geoId</i>, <i>name</i>, <i>address</i>, <i>description</i>, <i>userId</i>, <i>email</i>, <i>categoryId</i>, <i>site</i>, <i>created_at</i>, <i>updated_at</i>, <i>longitude</i> and <i>distance</i>.
+	    	</p>
 	    	<p class="quote">"url": "http://95.85.15.210/places/<strong>LATITUDE</strong>/<strong>LONGITUDE</strong>",</p>
-	    	<p><strong>getPlaceById</strong>: Get place by ID, you only need to pass the ID<br></p>
-	    	<p class="quote">"url": "http://95.85.15.210/places/1",</p>
-	    	<p><strong>getPlacesByCategory</strong>: Same as <strong>getPlaces</strong>, but with  category ID</p>
+
+	    	<p>
+	    		<strong>getPlaceById</strong><br>
+	    		Get place by ID, you only need to pass the ID<br>
+	    		Returns place:<br>
+	    		<i>id</i>, <i>foursquareId</i>, <i>geoId</i>, <i>name</i>, <i>address</i>, <i>description</i>, <i>userId</i>, <i>email</i>, <i>categoryId</i>, <i>site</i>, <i>created_at</i>, <i>updated_at</i>, <i>longitude</i> and <i>distance</i>.
+	    	</p>
+	    	<p class="quote">"url": "http://95.85.15.210/places/<strong>PLACE_ID</strong>",</p>
+
+	    	<p>
+	    		<strong>getPlacesByCategory</strong><br>
+	    		Same as <strong>getPlaces</strong>, but with category ID<br>
+	    		Returns all places with category ID:<br>
+	    		<i>id</i>, <i>foursquareId</i>, <i>geoId</i>, <i>name</i>, <i>address</i>, <i>description</i>, <i>userId</i>, <i>email</i>, <i>categoryId</i>, <i>site</i>, <i>created_at</i>, <i>updated_at</i>, <i>longitude</i> and <i>distance</i>.
+	    	</p>
 	    	<p class="quote">"url": "http://95.85.15.210/places/getPlacesByCategory/<strong>CATEGORY_ID</strong>/<strong>LATITUDE</strong>/<strong>LONGITUDE</strong>",</p>
+	    	
 	    	<hr>
 
 	    	<h2>Check-in</h2>
-	    	<p><strong>getLatestCheckin</strong>: get the latest created checkins</p>
+
+	    	<p>
+	    		<strong>getLatestCheckin</strong><br>
+	    		get the latest created checkins
+	    	</p>
 	    	<p class="quote">"url": "http://95.85.15.210/checkin/latest/<strong>USER_ID</strong>",</p>
-	    	<p><strong>getRecentCheckins</strong>: get the latest updated checkins</p>
+
+	    	<p>
+	    		<strong>getRecentCheckins</strong><br>
+	    		get the latest updated checkins
+	    	</p>
 	    	<p class="quote">"url": "http://95.85.15.210/checkin/recent/<strong>USER_ID</strong>",</p>
+
 	    	<hr>
 
 	    	<h2>Basic usage - PUT</h2>
