495dc4d7b2189288d91c5becad5fce93e16953c1
AngryMoustache <mrclotion@hotmail.com>
2016-07-19 11:11:22 +0200

    better routing

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -33,21 +33,21 @@ Route::get('auth/loginCallback/{client}', 'Auth\AuthController@loginCallback');
 
 //Route::group(['middleware' => ['jwt.auth']], function () {
     //Place
-    Route::get('/places/trending', 'PlaceController@getTrendingPlaces');
-    Route::get('/places/favourite', 'FavouriteController@getFavouritePlaces');
-    Route::get('/places/{id}/photos','PhotoController@getPictures')->where('id', '[0-9]+');
-    Route::get('/places/{lat}/{lng}','PlaceController@getPlaces');
-    Route::get('/places/{id}','PlaceController@getPlaceById')->where('id', '[0-9]+');
     Route::get('/places/getPlacesByCategory/{categoryId}/{lat}/{lng}','PlaceController@getPlacesByCategory')->where('categoryId', '[0-9]+');
-    Route::put('/places/add','PlaceController@store');
+    Route::get('/places/{id}/photos','PhotoController@getPictures')->where('id', '[0-9]+');
     Route::post('/places/{id}/addToFavourites','FavouriteController@addToFavourites')->where('id', '[0-9]+');
     Route::post('/places/{id}/removeFromFavourites','FavouriteController@removeFromFavourites')->where('id', '[0-9]+');
     Route::post('/places/{id}/uploadPhoto','PhotoController@uploadPhoto')->where('id','[0-9]+');
+    Route::get('/places/{lat}/{lng}','PlaceController@getPlaces');
+    Route::get('/places/trending', 'PlaceController@getTrendingPlaces');
+    Route::get('/places/favourite', 'FavouriteController@getFavouritePlaces');
+    Route::put('/places/add','PlaceController@store');
+    Route::get('/places/{id}','PlaceController@getPlaceById')->where('id', '[0-9]+');
 
     //Checkin
-    Route::put('/checkin','CheckinController@store');
     Route::get('/checkin/latest','CheckinController@getLatestCheckin');
     Route::get('/checkin/recent','CheckinController@getRecentCheckins');
+    Route::put('/checkin','CheckinController@store');
 
     // Users
     Route::get('/user/{id}','UserController@getUserById')->where('id', '[0-9]+');
