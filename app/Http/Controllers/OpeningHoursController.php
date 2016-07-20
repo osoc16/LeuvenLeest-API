c75d710a6f7988b1581967ad7def71f358f3b4a9
Jasper De Rocker <jasper.derocker@gmail.com>
2016-07-20 15:16:42 +0200

    added function to add openingHours to a place

--- app/Http/Controllers/OpeningHoursController.php
+++ app/Http/Controllers/OpeningHoursController.php
@@ -10,8 +10,10 @@ use App\OpeningHours;
 class OpeningHoursController extends Controller
 {
 
+    
     const MINUTES_PER_HOUR = 60;
-    const MINUTES_PER_TIMESLOT = 15;
+    const DIVISION_UNIT = 15; //quarters of hours
+    const HOURS_IN_A_DAY = 24;
 
     public function getOpeningHours($id){
         $hours = [];
@@ -29,11 +31,11 @@ class OpeningHoursController extends Controller
                 for($charnum = 0; $charnum < strlen($hourString); $charnum++){
                     $character = substr($hourString,$charnum,1);
                     if(!$open && $character === '1'){
-                        $begin = sprintf("%'.02d:%'.02d",intdiv(($charnum)*self::MINUTES_PER_TIMESLOT,self::MINUTES_PER_HOUR),(($charnum)*self::MINUTES_PER_TIMESLOT)%self::MINUTES_PER_HOUR);
+                        $begin = sprintf("%'.02d:%'.02d",intdiv(($charnum)*self::DIVISION_UNIT,self::MINUTES_PER_HOUR),(($charnum)*self::DIVISION_UNIT)%self::MINUTES_PER_HOUR);
                         $open = TRUE;
                     } else if($open && ($character === '0' || $charnum == strlen($hourString)-1)){
                         $charnum = $charnum == strlen($hourString)-1 ? $charnum+1 : $charnum;
-                        $end = sprintf("%'.02d:%'.02d",intdiv(($charnum)*self::MINUTES_PER_TIMESLOT,self::MINUTES_PER_HOUR),(($charnum)*self::MINUTES_PER_TIMESLOT)%self::MINUTES_PER_HOUR);
+                        $end = sprintf("%'.02d:%'.02d",intdiv(($charnum)*self::DIVISION_UNIT,self::MINUTES_PER_HOUR),(($charnum)*self::DIVISION_UNIT)%self::MINUTES_PER_HOUR);
                         array_push($hoursThisDay, $begin.' - '.$end);
                         $open = FALSE;
                     }
@@ -44,7 +46,40 @@ class OpeningHoursController extends Controller
         return $hours;
     }
 
-    public function addOpeningHours($id){
-
+    public function addOpeningHours($id, Request $request){
+        if(property_exists($requesst, 'timeframes')){
+            try{
+                foreach($request->timeframes as $timeframe){
+                    foreach($timeframe->days as $day){
+                        $openinghours = new OpeningHours();
+                        $openinghours->placeId = $place->id;
+                        $openinghours->dayOfWeek = $day;
+                        $hourstring = '';
+                        foreach($timeframe->open as $open){
+                            $starthour =  intdiv(intval($open->start),100);
+                            $startminutes = intval($open->start)%100;
+                            $bitstart = (($starthour*self::MINUTES_PER_HOUR)+$startminutes)/self::DIVISION_UNIT;
+                            $hourstring .= str_repeat('0', $bitstart-strlen($hourstring));
+                            $endhour = intdiv(intval($open->end),100);
+                            $endminutes = intval($open->end)%100;
+                            if($endhour == 0 && $endminutes == 0){
+                                $endhour = 24;
+                            }
+                            $bitend = (($endhour*self::MINUTES_PER_HOUR)+$endminutes)/self::DIVISION_UNIT;
+                            $hourstring .= str_repeat('1', $bitend-$bitstart);
+                        }
+                        $remainder = $totalHourStringLength-strlen($hourstring);
+                        $hourstring .= str_repeat('0', $remainder);
+                        $openinghours->hours = $hourstring;
+                        $openinghours->save();
+                    }
+                }
+                return new Response('openingHours added.',201);
+            } catch(Exception $ex){
+                return new Response('We could not add the openinghours.',500);
+            }
+        } else {
+            return new Response('No timeframes received.',400);
+        }
     }
 }
