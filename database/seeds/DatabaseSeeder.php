91d542cb6a3810759e182b1135fbe3c970ad35e5
Jasper De Rocker <jasper.derocker@gmail.com>
2016-07-20 10:23:09 +0200

    made link to foursquare-API shorter openinghour seeder ready for testing

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Http\Request;
 use App\Place;
+use App\OpeningHours;
 use Carbon\Carbon;
 
 class DatabaseSeeder extends Seeder
@@ -18,6 +19,7 @@ class DatabaseSeeder extends Seeder
         $this->call(GeolocationSeeder::class);
         $this->call(CategorySeeder::class);
         $this->call(PlaceSeeder::class);
+        $this->call(OpeningHoursSeeder::class);
     }
 }
 
@@ -107,12 +109,7 @@ class PlaceSeeder extends Seeder
             'https://api.foursquare.com/v2/venues/'.$venue->id
             .'?client_id='.env('FOURSQUARE_CLIENT_ID','')
             .'&client_secret='.env('FOURSQUARE_CLIENT_KEY','')
-            .'&ll=50.8798,4.7005'
-            .'&v=20150806&m=foursquare'
-            .'&radius=5000'
-            .'&intent=checkin'
-            .'&categoryId='.$this::CATEGORY_IDS
-            .'&near='.$this::LOCATION))->response->venue;
+            .'&v=20150806&m=foursquare'))->response->venue;
 
 
             $geo = DB::table('geolocations')->where('longitude',$fullvenue->location->lng)
@@ -141,6 +138,9 @@ class PlaceSeeder extends Seeder
 
 class OpeningHoursSeeder extends Seeder
 {
+    const MINUTES_PER_HOUR = 60;
+    const DIVISION_UNIT = 15; //quarters of hours
+    const HOURS_IN_A_DAY = 24;
     /**
      * Run the database seeds.
      *
@@ -148,9 +148,42 @@ class OpeningHoursSeeder extends Seeder
      */
     public function run()
     {
+        $totalHourStringLength = self::HOURS_IN_A_DAY*(self::MINUTES_PER_HOUR/self::DIVISION_UNIT)-1;
         $places = DB::table('places')->get();
         foreach($places as $place){
+            $hours = json_decode(file_get_contents(
+                'https://api.foursquare.com/v2/venues/'.$place->foursquareId.'/hours'
+                .'?client_id='.env('FOURSQUARE_CLIENT_ID','')
+                .'&client_secret='.env('FOURSQUARE_CLIENT_KEY','')
+                .'&v=20150806&m=foursquare'))->response->hours;
 
+            if(property_exists($hours, 'timeframes')){
+                foreach($hours->timeframes as $timeframe){
+                    foreach($timeframe->days as $day){
+                        $openinghours = new OpeningHours();
+                        $openinghours->placeId = $place->id;
+                        $openinghours->dayOfWeek = $day;
+                        $hourstring = '';
+                        foreach($timeframe->open as $open){
+                            $starthour =  intdiv(intval($open->start),100);
+                            $startminutes = intval($open->start)%100;
+                            $bitstart = (($starthour*self::MINUTES_PER_HOUR)+$startminutes)/self::DIVISION_UNIT;
+                            $hourstring .= str_repeat('0', $bitstart-strlen($hourstring)-1);
+                            $endhour = intdiv(intval($open->end),100);
+                            $endminutes = intval($open->end)%100;
+                            if($endhour == 0 && $endminutes == 0){
+                                $endhour = 23;
+                                $endminutes = 45;
+                            }
+                            $bitend = (($endhour*self::MINUTES_PER_HOUR)+$endminutes)/self::DIVISION_UNIT;
+                            $hourstring .= str_repeat('1', $bitend-$bitstart-1);
+                        }
+                        $hourstring .= str_repeat('0', $totalHourStringLength-strlen($hourstring));
+                        $openinghours->hours = $hourstring;
+                        $openinghours->save();
+                    }
+                }
+            }
         }
     }
 }
