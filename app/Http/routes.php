0cd3aa9b997a7ec036a2016a24c96466f22b81ec
thibautvincent <thibautvincent@icloud.com>
2016-07-22 10:53:34 +0200

    Fix

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,7 +14,7 @@ use Illuminate\Http\Request;
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Credentials: true');
 header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
-header('Access-Control-Allow-Methods: POST, GET, PUT, ACTIONS')
+header('Access-Control-Allow-Methods: POST, GET, PUT, ACTIONS');
 
 
 Route::get('/', function () {
