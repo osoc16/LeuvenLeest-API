9421aa5b05c3cc414e1e9070e5d26f4602fb6308
Jasper De Rocker <jasper.derocker@gmail.com>
2016-07-25 10:42:10 +0200

    Updated databaseseeder geolocation is added by placeseeder added new locations that were requested by the partner

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,7 +16,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        $this->call(GeolocationSeeder::class);
         $this->call(CategorySeeder::class);
         $this->call(PlaceSeeder::class);
         $this->call(OpeningHoursSeeder::class);
@@ -29,7 +28,6 @@ class GeolocationSeeder extends Seeder
 
     const ENDPOINT_URL = 'https://api.foursquare.com/v2/venues/search';
     const LOCATION = 'Leuven';
-    const CATEGORY_IDS = '4bf58dd8d48988d163941735,4bf58dd8d48988d1e0931735,4bf58dd8d48988d1a7941735,4bf58dd8d48988d12f941735';
     /**
      * Run the database seeds.
      *
@@ -37,25 +35,28 @@ class GeolocationSeeder extends Seeder
      */
     public function run()
     {
-        $response = json_decode(file_get_contents(
-            $this::ENDPOINT_URL
-            .'?client_id='.env('FOURSQUARE_CLIENT_ID','')
-            .'&client_secret='.env('FOURSQUARE_CLIENT_KEY','')
-            .'&ll=50.8798,4.7005'
-            .'&v=20150806&m=foursquare'
-            .'&radius=5000'
-            .'&intent=checkin'
-            .'&categoryId='.$this::CATEGORY_IDS
-            .'&near='.$this::LOCATION))->response;
+        $categories = ['4bf58dd8d48988d163941735','4bf58dd8d48988d1e0931735','4bf58dd8d48988d1a7941735','4bf58dd8d48988d12f941735'];
+        foreach($categories as $category){  
+            $response = json_decode(file_get_contents(
+                $this::ENDPOINT_URL
+                .'?client_id='.env('FOURSQUARE_CLIENT_ID','')
+                .'&client_secret='.env('FOURSQUARE_CLIENT_KEY','')
+                .'&ll=50.8798,4.7005'
+                .'&v=20150806&m=foursquare'
+                .'&radius=5000'
+                .'&intent=checkin'
+                .'&categoryId='.$category
+                .'&near='.$this::LOCATION))->response;
 
-        $venues = $response->venues;
+            $venues = $response->venues;
 
-        foreach($venues as $venue){
-            $exists = DB::table('geolocations')->where('longitude',$venue->location->lng)
-                ->where('latitude',$venue->location->lat)->get();
-            if(!$exists){
-                DB::table('geolocations')->insert([['latitude' => $venue->location->lat,
-                                                    'longitude' => $venue->location->lng]]);
+            foreach($venues as $venue){
+                $exists = DB::table('geolocations')->where('longitude',$venue->location->lng)
+                    ->where('latitude',$venue->location->lat)->get();
+                if(!$exists){
+                    DB::table('geolocations')->insert([['latitude' => $venue->location->lat,
+                                                        'longitude' => $venue->location->lng]]);
+                }
             }
         }
     }
@@ -83,7 +84,6 @@ class PlaceSeeder extends Seeder
 
     const ENDPOINT_URL = 'https://api.foursquare.com/v2/venues/search';
     const LOCATION = 'Leuven';
-    const CATEGORY_IDS = '4bf58dd8d48988d163941735,4bf58dd8d48988d1e0931735,4bf58dd8d48988d1a7941735,4bf58dd8d48988d12f941735';
     /**
      * Run the database seeds.
      *
@@ -91,49 +91,101 @@ class PlaceSeeder extends Seeder
      */
     public function run()
     {
-        $response = json_decode(file_get_contents(
-            $this::ENDPOINT_URL
-            .'?client_id='.env('FOURSQUARE_CLIENT_ID','')
-            .'&client_secret='.env('FOURSQUARE_CLIENT_KEY','')
-            .'&ll=50.8798,4.7005'
-            .'&v=20150806&m=foursquare'
-            .'&radius=5000'
-            .'&intent=checkin'
-            .'&categoryId='.$this::CATEGORY_IDS
-            .'&near='.$this::LOCATION))->response;
+        $categories = ['4bf58dd8d48988d163941735','4bf58dd8d48988d1e0931735','4bf58dd8d48988d1a7941735','4bf58dd8d48988d12f941735'];
+        foreach($categories as $category){
+            $response = json_decode(file_get_contents(
+                $this::ENDPOINT_URL
+                .'?client_id='.env('FOURSQUARE_CLIENT_ID','')
+                .'&client_secret='.env('FOURSQUARE_CLIENT_KEY','')
+                .'&ll=50.8798,4.7005'
+                .'&v=20150806&m=foursquare'
+                .'&radius=5000'
+                .'&intent=checkin'
+                .'&categoryId='.$category
+                .'&near='.$this::LOCATION
+                .'&limit=50'))->response;
 
-        $venues = $response->venues;
+            $venues = $response->venues;
 
-        foreach($venues as $venue){
+            foreach($venues as $venue){
 
-            $fullvenue = json_decode(file_get_contents(
-            'https://api.foursquare.com/v2/venues/'.$venue->id
-            .'?client_id='.env('FOURSQUARE_CLIENT_ID','')
-            .'&client_secret='.env('FOURSQUARE_CLIENT_KEY','')
-            .'&v=20150806&m=foursquare'))->response->venue;
+                $fullvenue = json_decode(file_get_contents(
+                'https://api.foursquare.com/v2/venues/'.$venue->id
+                .'?client_id='.env('FOURSQUARE_CLIENT_ID','')
+                .'&client_secret='.env('FOURSQUARE_CLIENT_KEY','')
+                .'&v=20150806&m=foursquare'))->response->venue;
+                
+                $exists = DB::table('geolocations')->where('longitude',$venue->location->lng)
+                    ->where('latitude',$venue->location->lat)->get();
+                if(!$exists){
+                    DB::table('geolocations')->insert([['latitude' => $venue->location->lat,
+                                                        'longitude' => $venue->location->lng]]);
+                }
 
+                $geo = DB::table('geolocations')->where('longitude',$fullvenue->location->lng)
+                    ->where('latitude',$fullvenue->location->lat)->first();
 
-            $geo = DB::table('geolocations')->where('longitude',$fullvenue->location->lng)
-                ->where('latitude',$fullvenue->location->lat)->first();
+                $catfoursquareid = $fullvenue->categories[0]->id;
+                $description = property_exists($fullvenue,'description') ? $fullvenue->description : '';
+                $category = DB::table('categories')->where('foursquareId',$catfoursquareid)->first();
+                $categoryid = is_object($category) ? $category->id : null;
+                $url = property_exists($fullvenue, 'url') ? $fullvenue->url : '';
+                $description = property_exists($fullvenue, 'description') ? $fullvenue->description : '';
 
-            $catfoursquareid = $fullvenue->categories[0]->id;
-            $description = property_exists($fullvenue,'description') ? $fullvenue->description : '';
-            $category = DB::table('categories')->where('foursquareId',$catfoursquareid)->first();
-            $categoryid = is_object($category) ? $category->id : null;
-            $url = property_exists($fullvenue, 'url') ? $fullvenue->url : '';
-            $description = property_exists($fullvenue, 'description') ? $fullvenue->description : '';
-
-            $place = new Place();
-            $place->name = $fullvenue->name;
-            $place->geoId = $geo->id;
-            $place->foursquareId = $fullvenue->id;
-            $place->description = $description;
-            $place->email = '';
-            $place->site = $url;
-            $place->categoryId = $categoryid;
-            $place->address = $fullvenue->location->formattedAddress[0];
-            $place->save();
+                $place = new Place();
+                $place->name = $fullvenue->name;
+                $place->geoId = $geo->id;
+                $place->foursquareId = $fullvenue->id;
+                $place->description = $description;
+                $place->email = '';
+                $place->site = $url;
+                $place->categoryId = $categoryid;
+                $place->address = $fullvenue->location->formattedAddress[0];
+                $place->save();
+            }
         }
+
+        //Add places that were tipped by cultuurconnect: abdij Keizersberg; dijlepark; dijleterassen
+        //park den bruul; hogeschoolplein; sint-geertrui abdij
+        $neededvenueIds = ['4ca5ea3914c337047240ba3b','4ddac32db0fb97d348b86ec3','4c28ebb09eb1952164a12959','4c379cb9dfb0e21e0e1dada8','4bf1725f408ec9287de66600','54cd4d0f498e263faa9f1571'];
+        foreach($neededvenueIds as $id){
+            $fullvenue = json_decode(file_get_contents(
+                'https://api.foursquare.com/v2/venues/'.$id
+                .'?client_id='.env('FOURSQUARE_CLIENT_ID','')
+                .'&client_secret='.env('FOURSQUARE_CLIENT_KEY','')
+                .'&v=20150806&m=foursquare'))->response->venue;
+
+            $placeExists = DB::table('places')->where('foursquareId',$id)->get();
+            if(!$placeExists){
+                $exists = DB::table('geolocations')->where('longitude',$fullvenue->location->lng)
+                        ->where('latitude',$fullvenue->location->lat)->get();
+                if(!$exists){
+                    DB::table('geolocations')->insert([['latitude' => $fullvenue->location->lat,
+                                                        'longitude' => $fullvenue->location->lng]]);
+                }
+
+                $geo = DB::table('geolocations')->where('longitude',$fullvenue->location->lng)
+                    ->where('latitude',$fullvenue->location->lat)->first();
+                $catfoursquareid = $fullvenue->categories[0]->id;
+                $description = property_exists($fullvenue,'description') ? $fullvenue->description : '';
+                $category = DB::table('categories')->where('foursquareId',$catfoursquareid)->first();
+                $categoryid = is_object($category) ? $category->id : null;
+                $url = property_exists($fullvenue, 'url') ? $fullvenue->url : '';
+                $description = property_exists($fullvenue, 'description') ? $fullvenue->description : '';
+
+                $place = new Place();
+                $place->name = $fullvenue->name;
+                $place->geoId = $geo->id;
+                $place->foursquareId = $fullvenue->id;
+                $place->description = $description;
+                $place->email = '';
+                $place->site = $url;
+                $place->categoryId = $categoryid;
+                $place->address = $fullvenue->location->formattedAddress[0];
+                $place->save();
+            }
+        }
+
     }
 }
 
