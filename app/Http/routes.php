26af22a9ebdfaa48285ae79e0b1c6f56895a1799
thibautvincent <thibautvincent@icloud.com>
2016-07-26 14:12:40 +0200

    Fix

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,7 +15,7 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Credentials: true');
 header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
 header('Access-Control-Allow-Methods: POST, GET, PUT');
-header('Access-Control-Expose-Headers', 'Authorization')
+header('Access-Control-Expose-Headers', 'Authorization');
 
 
 Route::get('/', function () {
