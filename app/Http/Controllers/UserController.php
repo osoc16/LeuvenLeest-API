d7a77bfb05de90bee4b77366f1b0813a28776904
thibautvincent <thibautvincent@icloud.com>
2016-07-26 16:28:00 +0200

    Fix

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -26,7 +26,7 @@ class UserController extends Controller
 
     public function getAccountDetails()
     {
-        $user = JWTAuth::toUser(JWTAuth::parseToken());
+        $user = JWTAuth::toUser(JWTAuth::getToken());
 
         if ($user)
         {
