e1c8eee6e860c11f4e2f82daf7c1c97ab97e72eb
thibautvincent <thibautvincent@icloud.com>
2016-07-26 14:07:25 +0200

    Change expose headers

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -19,6 +19,7 @@ class Cors
             return response('', 200)
                 ->header('Allow_Origin', '*')
               ->header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE')
+              ->header('Access-Control-Expose-Headers', 'Authorization')
               ->header('Access-Control-Allow-Headers', 'accept, content-type,x-xsrf-token, x-csrf-token, authorization, cache-control', 'X-Requested-With', 'Origin'); // Add any required headers here
         }
         return $next($request);
