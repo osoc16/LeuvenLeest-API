6bb82ece32b2eaf2ceca1614e5b01faa277e1506
thibautvincent <thibautvincent@icloud.com>
2016-07-26 13:05:59 +0200

    Remove OPTIONs

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -18,7 +18,7 @@ class Cors
         if ($request->isMethod('options')) {
             return response('', 200)
                 ->header('Allow_Origin', '*')
-              ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
+              ->header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE')
               ->header('Access-Control-Allow-Headers', 'accept, content-type,x-xsrf-token, x-csrf-token, authorization, cache-control', 'X-Requested-With', 'Origin'); // Add any required headers here
         }
         return $next($request);
